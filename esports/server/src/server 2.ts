b724613d2923fd82bbca1ed2c583289559290921
itIsV <soicinivt@gmail.com>
2022-09-14 20:51:07 -0300

    feat: day2 final

--- esports/server/src/server 2.ts
+++
@@ -1,9 +0,0 @@
-import  express from 'express';
-
-const app = express()
-
-app.get('/ads', (req, res) => {
-  return res.send('Acessou ads em, UHUUU!!\n hahahaHAHAHAH')
-});
-
-app.listen(3333);
--- esports/web/src/App.tsx
+++ esports/web/src/App.tsx
@@ -1,4 +1,5 @@
 import "./styles/main.css";
+import { MagnifyingGlassPlus } from "phosphor-react"
 import logo from "./assets/logo-nlw-esports.svg"
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
       BUAHAHAHAHAHAHAH
       </h1>
 
-      <div className="mas-w-[1344px max-auto flex flex-col items-center my-20]">\
+      <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">\
         <img src={logo}></img>
 
         <h1 className="text-3xl text-white font-black mt-20">
@@ -104,8 +105,26 @@ function App() {
             </div>
           </a>
         </div>
-      </div>
 
+        <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
+          <div className="bg-[#2a2634] px-8 py-6 flex justify-between items-center">
+            <div>
+              <strong className="text-2xl text-white font-black block">
+                Não encontrou o seu DUO?
+              </strong>
+
+              <span className="text-zinc-400 block">
+                Publique um anúncio para encontrar novos players!
+              </span>
+            </div>
+
+            <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex  items-center gap-3">
+              <MagnifyingGlassPlus size={ 24 }/>
+              Publicar anúncio
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
     )
 }
