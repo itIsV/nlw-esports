fbae0c0af24a238fcc62a7e91ecfbcbb593747fe
itIsV <soicinivt@gmail.com>
2022-09-14 21:41:50 -0300

    feat: day 3 part 1

--- esports/server/src/server.ts
+++ esports/server/src/server.ts
@@ -1,9 +1,34 @@
-import  express from 'express';
+import express from 'express';
 
 const app = express()
 
+app.get('/games', (req, res) => {
+  return res.json([])
+});
+
+app.get('/games/:id/ads', (req, res) => {
+  return res.json([
+    {
+      "id": 0,
+      "name": "beriseu",
+    },
+    {
+      "id": 1,
+      "name": "damasdco",
+    },
+    {
+      "id": 2,
+      "name": "Amadeus cho",
+    },
+  ])
+});
+
+app.post('/ads/:id/discord', (req, res) => {
+  return res.status(201).json('Acessou ads em, UHUUU!!\n hahahaHAHAHAH')
+});
+
 app.get('/ads', (req, res) => {
-  return res.send('Acessou ads em, UHUUU!!\n hahahaHAHAHAH')
+  return res.status(200).json('Acessou ads discord id em, UHUUU!!\n hahahaHAHAHAH')
 });
 
 app.listen(3333);
